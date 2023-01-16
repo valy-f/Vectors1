046515345306353cc5ce4f58d466b24da2e6fc84
valy-f <87707135+valy-f@users.noreply.github.com>
2023-01-16 07:00:36 +0100

    Update Moves.hpp

--- Moves.hpp
+++ Moves.hpp
@@ -3,27 +3,28 @@ class Move
 {  
     std::vector<int> x = {1,2,3,4,5,6};
     int n = 1;
+    int an = -1;
 public:
     
     void LeftMove()
     {
-        std::cout << "\nVector << n:\n";
+        std::cout << "\n\nVector << " << n << "\n";
         for (const int& i : x) {
             std::cout << (i << n) << "  ";
         }
     }
     void RightMove()
     {
-        std::cout << "\nVector >> n:\n";
+        std::cout << "\n\nVector >> " << n << "\n";
         for (const int& i : x) {
             std::cout << (i >> n) << "  ";
         }
     }
     void ArithmeticRight()
     {
-        std::cout << "\nVector arithmetic shift >> n:\n";
+        std::cout << "\n\nVector arithmetic shift >> " << n << "\n";
         for (const int& i : x) {
-            std::cout << (i >> n) << "  "; // nie wymyuslilam jeszcze
+            std::cout << (i >> an) << "  "; // nie wymyuslilam jeszcze
         }
     }
 
